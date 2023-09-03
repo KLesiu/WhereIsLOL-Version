e2523a066ce2495a37b041c9a58f59bd90efe8c1
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-09-03 22:40:26 +0200

    Add header and gameboard

---
+++ frontend/src/Components/Gameboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles/Gameboard.css'
+const Gameboard=(props)=>{
+    return(
+        <div className="imgHolder">
+            <img alt="gameboard" src="./wallpapers/wallpaper.jpg"></img>
+        </div>
+        
+    )
+}
+
+export default Gameboard
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Header.css'
+const Header=(props)=>{
+    return(
+        <header>
+            <h1>WhereIs Champion?</h1>
+            <button id='leaderboardButton'>Leaderboard</button>
+        </header>
+    )
+}
+
+export default Header
