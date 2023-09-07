209aaf168240b45127bb6698e24ce8b2492c6f03
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-09-07 22:19:29 +0200

    Add animation to images in basket

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -2,7 +2,16 @@ import React, { useEffect } from 'react'
 import './styles/Header.css'
 const Header=(props)=>{
    useEffect(()=>{
-    
+    const images=document.querySelectorAll(".champion")
+    images.forEach((ele)=>{
+        ele.addEventListener("click",()=>{
+            ele.classList.toggle('rotate')
+            const h3 = ele.parentElement.children[0]
+            
+            h3.classList.toggle('show')
+            
+        })
+    })
    })
     if(props.start===true){
         return(
@@ -10,16 +19,16 @@ const Header=(props)=>{
                 <h1>WhereIs Champion?</h1>
                 <button id="leaderboardButton">Leaderboard</button>
                 <ul className="championsBasket">
-                    <li><div className='glass'> <h3 className='hidden'>Akali</h3><img className='champion' src="./champions/Akali.jpg">
+                    <li><div className='glass'> <h3 className=''>Akali</h3><img className='champion' src="./champions/Akali.jpg">
                         </img></div>
                         </li>
-                    <li><div className='glass'> <h3 className='hidden'>Nocturne</h3>  <img className='champion' src="./champions/Nocturne.jpg"></img></div>
+                    <li><div className='glass'> <h3 className=''>Nocturne</h3>  <img className='champion' src="./champions/Nocturne.jpg"></img></div>
                        </li>
-                    <li><div className='glass'> <h3 className='hidden'>Riven </h3><img className='champion' src="./champions/Riven.jpg"></img></div>
+                    <li><div className='glass'> <h3 className=''>Riven </h3><img className='champion' src="./champions/Riven.jpg"></img></div>
                         </li>
-                    <li><div className='glass'><h3 className='hidden'>Vladimir</h3> <img className='champion' src="./champions/Vladimir.jpg"></img> </div>
+                    <li><div className='glass'><h3 className=''>Vladimir</h3> <img className='champion' src="./champions/Vladimir.jpg"></img> </div>
                         </li>
-                    <li><div className='glass'> <h3 className='hidden'>Ziggs</h3> <img className='champion' src="./champions/Ziggs.jpg"></img></div>
+                    <li><div className='glass'> <h3 className=''>Ziggs</h3> <img className='champion' src="./champions/Ziggs.jpg"></img></div>
                         </li>
                 </ul>
             </header>
