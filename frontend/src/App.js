fd9eb9692c181913de9868c43b3b79f9eedea6c7
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-09-07 18:04:12 +0200

    Add championBasket to game section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,25 @@ import Gameboard from './Components/Gameboard';
 import Header from './Components/Header';
 import PreGame from './Components/PreGame';
 import './index.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
   const [start,setStart]=useState(false)
+  useEffect(()=>{
+    document.querySelector('#startButton').addEventListener("click",()=>{
+      setStart(true)
+    })
+  },[])
   if(start===false){
     return(
       <div className='App'>
-        <Header/>
+        <Header start={false}/>
         <PreGame />
       </div>
     )
   }
   return (
     <div className="App">
-      <Header />
+      <Header start={true} />
       <Gameboard />
     </div>
   );
--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './styles/Header.css'
 const Header=(props)=>{
+   useEffect(()=>{
+    
+   })
+    if(props.start===true){
+        return(
+            <header>
+                <h1>WhereIs Champion?</h1>
+                <button id="leaderboardButton">Leaderboard</button>
+                <ul className="championsBasket">
+                    <li><div className='glass'> <h3 className='hidden'>Akali</h3><img className='champion' src="./champions/Akali.jpg">
+                        </img></div>
+                        </li>
+                    <li><div className='glass'> <h3 className='hidden'>Nocturne</h3>  <img className='champion' src="./champions/Nocturne.jpg"></img></div>
+                       </li>
+                    <li><div className='glass'> <h3 className='hidden'>Riven </h3><img className='champion' src="./champions/Riven.jpg"></img></div>
+                        </li>
+                    <li><div className='glass'><h3 className='hidden'>Vladimir</h3> <img className='champion' src="./champions/Vladimir.jpg"></img> </div>
+                        </li>
+                    <li><div className='glass'> <h3 className='hidden'>Ziggs</h3> <img className='champion' src="./champions/Ziggs.jpg"></img></div>
+                        </li>
+                </ul>
+            </header>
+        )
+    }
     return(
         <header>
             <h1>WhereIs Champion?</h1>
